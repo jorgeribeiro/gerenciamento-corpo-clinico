17dd94aca9f809d4f4803cf7e090513174ffeb29
Jorge Ribeiro <joorgemelo@gmail.com>
2019-07-31 23:58:01 -0300

    Changes to be committed: 	modified:   gerenciador/src/App.js

--- gerenciador/src/App.js
+++ gerenciador/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import doctor from './assets/doctor.svg';
 import './App.css';
 
-import "font-awesome/css/font-awesome.css";
 
 function App() {
   return (
@@ -12,7 +11,7 @@ function App() {
         <p>
           Gerenciamento de Corpo Clínico
         </p>
-        <input type="text" placeholder="E-mail" />
+        <input type="text" placeholder="Endereço de e-mail" />
         <input type="password" placeholder="Senha" />
         <button>Entrar</button>        
         <button>Credenciamento</button>
